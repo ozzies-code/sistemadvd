166cf0c37b521a9ffd5449b40603b2ab36ccaa99
Oswaldo Jesús Marín Pagés <83247451+ozzies-code@users.noreply.github.com>
2023-01-17 03:05:34 -0400

    Código del Proyecto SIGIDVD
    Se agrega el código inicial del proyecto: HTML, CSS y PHP.

---
+++ sigidvd.php
@@ -0,0 +1,72 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+  <title>Sistema de Gestión de DVD</title>
+  <meta charset="utf-8">
+  
+  <link rel="stylesheet" type="text/css" href="style/style.css" title="style" />
+</head>
+
+<body>
+  <div id="main" style="margin-top: 100px;">
+    <div id="site_content">
+      <div id="content">
+        <!-- insert the page content here -->
+  <center>
+        <h1>Sistema DVD</h1>
+     <form name="form1" method="post" action="busquedach.php">
+       <table align="center"  border="0">
+       <tr><td width="294">
+				 <div class="stl_anchoCompleto3">  
+            		<br>
+					<table  border="0" align="center">
+						<tr height="25">
+							<td align="right">
+								C&oacute;digo:&nbsp;&nbsp;
+							</td>
+							<td>
+								<input name="codigo" maxlength="8" id="codigo">
+							</td>
+						</tr>
+						<tr height="25">
+							<td align="right">
+								Usuario:&nbsp;&nbsp;
+							</td>
+							<td>
+								<select size="1" name="usuario">
+									<optgroup label=""><option value="Oswaldo Marin">Oswaldo Marin </option><option value="Carlos Gutierrez">Carlos Gutierrez</option> </optgroup>	        
+                                </select>
+							</td>
+						</tr>
+					</table>
+                  
+                    
+            <br />
+      	    <input type="submit" value="CONSULTAR DVD" name="Submit" style="font-family: Verdana; font-size: 8 pt">
+          	
+        </div>
+	</td></tr>
+    </table>
+    <table width="85%" align="center">
+        <tr>
+       	 <td align="center"><b>Recuerde: </b>Este proyecto está en construcción</b></td>
+        </tr>
+    </table>
+</form>
+        
+
+
+  </center>
+    
+      </div>
+    </div>
+  </div>
+<div id="content_footer"></div>
+    <div id="footer">
+    
+    </div>
+  </div>
+</body>
+</html>
+
